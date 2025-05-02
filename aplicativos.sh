530e39d152fc7129d1d30330f857cc7082f767c3
Robson Cordeiro <69126659+robsonpc@users.noreply.github.com>
2025-05-02 12:14:02 -0300

    Update aplicativos.sh

--- aplicativos.sh
+++ aplicativos.sh
@@ -1,54 +1,5 @@
 #!/bin/bash
 
-https://github.com/TheAssassin/AppImageLauncher/releases/
-
-https://librewolf-community.gitlab.io/
-
-[http://freshwiki.manjaro.org/index.php/Virt-manager](https://wiki.manjaro.org/index.php/Virt-manager)
-
-
-## Criar Pool VM - Linha comando
-$ virsh pool-create-as --name test-dir --type dir --target /home/
-
-$ sudo dd if=debian.iso of=/dev/sdb bs=16M oflag=sync status=progress
-
-## Apps para instalação 
-sudo apt install gamemode pdfarrnger telegram-desktop geary timeshift neofetch keepassxc chromium linux-headers-amd64 libvulkan1 libvulkan1:i386 mpv
-
-1 - Melhorar Aparencia LibreOffice: sudo apt install libreoffice-gtk3  
-
-  1.1 - Estilo diferente: sudo apt install libreoffice-style-sifr
-
-2 - Biblioteca Firefox (Se necessario): sudo apt install libdbus-glib-1-2
-
-3 - Instalação VirtualBox
-
-curl https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --dearmor > oracle_vbox_2016.gpg
-
-curl https://www.virtualbox.org/download/oracle_vbox.asc | gpg --dearmor > oracle_vbox.gpg
-
-sudo install -o root -g root -m 644 oracle_vbox_2016.gpg /etc/apt/trusted.gpg.d/
-
-sudo install -o root -g root -m 644 oracle_vbox.gpg /etc/apt/trusted.gpg.d/
-
-echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-
-sudo apt update
-
-sudo apt install linux-headers-$(uname -r) dkms
-
-sudo apt-get install virtualbox-7.1 -y
-
-sudo /sbin/vboxconfig
-
-## Interfaces
-1 - MATE: sudo apt install mate-tweak caja-open-terminal caja-wallpaper caja-admin
-
-2 - KDE minimal: sudo apt install sddm plasma-desktop plasma-pa dolphin dolphin-plugins konsole
-
-
-
-
 ## Instação de aplicativos Flatpak
 
 sudo apt install flatpak
@@ -65,13 +16,3 @@ flatpak install flathub org.mozilla.Thunderbird
 flatpak install flathub com.vscodium.codium
 flatpak install flathub io.github.spacingbat3.webcord
 flatpak uninstall --unused
-
-
-## Limpar tabela uefi
-## sudo efibootmgr -b xxxx -B
-
-## Driver Windows
-## driverhub
-
-## Pesquisa Steam
-## sed -i -e '/\"SurveyDate\"/ s/"[0-9].*"/"'$(date +%Y-%m-%d -d "2 years ago")'"/' ~/.local/share/Steam/config/config.vdf
